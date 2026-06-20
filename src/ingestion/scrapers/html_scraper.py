69b7f5b38f643e449dfcc9fef372902dc950c2f3
michael <>
2026-06-20 11:22:00 -0400

    Added bs4, fixed scraper, added offline testing

--- src/ingestion/scrapers/html_scraper.py
+++ src/ingestion/scrapers/html_scraper.py
@@ -1,24 +1,14 @@
 import httpx
 from bs4 import BeautifulSoup
 
-def scrape(url: str) -> tuple[str, str | None]:
-    """Fetch a URL and extract clean text + title.
-
-    Returns (text, title). Title may be None if not found.
-    """
-    with httpx.Client(timeout=30, follow_redirects=True) as client:
-        response = client.get(url)
-        response.raise_for_status()
-        
-    html = response.text
-
+def _extract_from_html(html: str) -> tuple[str, str | None]:
     cut_marker = "<!-- close main-wrapper"
     if cut_marker in html:
         html = html.split(cut_marker)[0]
 
     html = BeautifulSoup(html, "html.parser")
 
-    for el in html.select(".footer, .global-nav, .navigation, .topbar, .content__meta"): # removing elements by class
+    for el in html.select(".footer, .global-nav, .navigation, .topbar, .content__meta, .visuallyhidden"): # removing elements by class
         el.decompose()
 
     for tag in html(["header","footer","nav"]): # removing elements by tag
@@ -36,13 +26,18 @@ def scrape(url: str) -> tuple[str, str | None]:
 
     text = html.get_text("\n", strip=True)
     
-    title = str(html("title")[0])
+    title = (html("title")[0]).get_text()
 
     return text, title
 
-_url = "https://carleton.ca/registration/new-ug/new-student-faqs/"
-_url2 = "https://carleton.ca/scs/current-students/bachelor-of-cybersecurity/bcyber-courses-and-registration/"
 
-with open("output.txt", "w", encoding="utf-8") as file:
-    for i in scrape(_url2):
-        file.write(i)
+def scrape(url: str) -> tuple[str, str | None]:
+    """Fetch a URL and extract clean text + title.
+
+    Returns (text, title). Title may be None if not found.
+    """
+    with httpx.Client(timeout=30, follow_redirects=True) as client:
+        response = client.get(url)
+        response.raise_for_status()
+        
+    return _extract_from_html(response.text)
